6bdace61220738f03d7d963c2e70649441d27ab3
Jisung <yamsiri@gmail.com>
2025-07-24 10:22:10 +0900

    refactor: Image 컴포넌트 조건부 렌더링 로직 단순화
    - Gemini Code Assist 피드백에 따라 불필요한 조건부 렌더링 제거
    - 항상 picture 태그 구조 사용으로 코드 단순화 및 가독성 향상
    - dev import 제거 및 코드 정리

--- src/lib/components/content/Image.svelte
+++ src/lib/components/content/Image.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import { dev } from '$app/environment'
-
   export let src: string
   export let alt: string
   export let width: number | string | undefined = undefined
@@ -8,11 +6,9 @@
   export let sizes: string | undefined = undefined
   export let style: string | undefined = undefined
 
-  // vite-imagetools can't handle external URLs, so we just use a regular img tag for them
-  const isExternal: boolean = src.startsWith('http')
-
   // To prevent layout shift, we need to know the dimensions of the image.
   // For local images, try to get dimensions from the path if specified in format `image@<width>x<height>.jpg`
+  const isExternal: boolean = src.startsWith('http')
   if (width === undefined && height === undefined && !isExternal) {
     const match: RegExpMatchArray | null = src.match(/@(\d+)x(\d+)\./)
     if (match) {
@@ -27,13 +23,9 @@
   const img = { src }
 </script>
 
-{#if dev || isExternal}
-  <img {src} {alt} {width} {height} {sizes} {style} loading="lazy" {...$$restProps} />
-{:else}
-  <picture>
-    {#each Object.entries(sources) as [format, source]}
-      <source type={`image/${format}`} srcset={String(source)} {sizes} />
-    {/each}
-    <img {...img} {alt} {width} {height} {sizes} {style} loading="lazy" {...$$restProps} />
-  </picture>
-{/if}
+<picture>
+  {#each Object.entries(sources) as [format, source]}
+    <source type={`image/${format}`} srcset={String(source)} {sizes} />
+  {/each}
+  <img {...img} {alt} {width} {height} {sizes} {style} loading="lazy" {...$$restProps} />
+</picture>
