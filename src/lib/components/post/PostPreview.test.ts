db29e017475708a06ad8eac4b9500870d6f7a83f
NANGGO <yamsiri@gmail.com>
2025-11-17 20:19:54 +0900

    perf(card): use viewport-based preloading (#60)
    * perf(card): use viewport-based preloading
    
    * test(post-preview): expect viewport preloading

--- src/lib/components/post/PostPreview.test.ts
+++ src/lib/components/post/PostPreview.test.ts
@@ -50,8 +50,8 @@ describe('PostPreview 컴포넌트', () => {
 
     const cardLink = document.querySelector('a[href="/post/test-post-slug"]')
     expect(cardLink).toBeInTheDocument()
-    expect(cardLink).toHaveAttribute('data-sveltekit-preload-data', 'hover')
-    expect(cardLink).toHaveAttribute('data-sveltekit-preload-code', 'hover')
+    expect(cardLink).toHaveAttribute('data-sveltekit-preload-data', 'viewport')
+    expect(cardLink).toHaveAttribute('data-sveltekit-preload-code', 'viewport')
   })
 
   it('최대 태그 개수만큼 태그가 표시된다', () => {
